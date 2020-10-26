abf7a4e61cd04a751c10ed1218d86c3f437aeaea
jhaynes921 <jhaynes921@att.net>
2020-10-26 11:03:14 -0500

    work proceeds

--- RandomTools/RandomTools/Program.cs
+++ RandomTools/RandomTools/Program.cs
@@ -19,7 +19,7 @@ namespace RandomTools
 			//Application.Run(new MetaDataTesting());
 			//Application.Run(new ReadButtons());
 			//Application.Run(new DirectoryTool());
-			Application.Run(new MergeTextFiles());
+			Application.Run(new Catalogger());
 		}
 	}
 }
